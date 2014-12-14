fcc8cc6433912938d786cfa58c3194d7ffc9af8e
Eugene Sharygin <eush77@gmail.com>
2014-12-14 16:19:45 +0300

    Start HTTP server

--- src/js/server.js
+++ src/js/server.js
@@ -5,13 +5,10 @@ var makeBoard = require('./game/newboard');
 var WebSocketServer = require('ws').Server
   , guid = require('guid').raw
   , fzip = require('fzip')
-  , extend = require('extend');
+  , extend = require('extend')
+  , StaticServer = require('node-static').Server;
 
-
-var port = 2020;
-var server = new WebSocketServer({ port: port });
-var clientQueue = [];
-var games = {};
+var http = require('http');
 
 
 var newClient = function (client) {
@@ -42,26 +39,55 @@ var startGame = function (alice, bob) {
 };
 
 
-server.on('connection', function (alice) {
-  newClient(alice);
+var createWsServer = function (port) {
+  var wsServer = new WebSocketServer({ port: port });
+  var clientQueue = [];
+  var games = {};
 
-  // Find an opponent.
-  while (clientQueue.length) {
-    var bob = clientQueue.shift();
-    if (bob.readyState != bob.OPEN) {
-      // Gone.
-      continue;
+  wsServer.on('connection', function (alice) {
+    newClient(alice);
+
+    // Find an opponent.
+    while (clientQueue.length) {
+      var bob = clientQueue.shift();
+      if (bob.readyState != bob.OPEN) {
+        // Gone.
+        continue;
+      }
+
+      games[alice.id] = bob;
+      games[bob.id] = alice;
+
+      return startGame(alice, bob);
     }
 
-    games[alice.id] = bob;
-    games[bob.id] = alice;
-
-    return startGame(alice, bob);
-  }
-
-  alice.send(JSON.stringify({ code: 'wait' }));
-  clientQueue.push(alice);
-});
+    alice.send(JSON.stringify({ code: 'wait' }));
+    clientQueue.push(alice);
+  });
+};
 
 
-console.log('Server started at localhost:' + port);
+var createHttpServer = function (port) {
+  var fileServer = new StaticServer('dist');
+
+  http.createServer(function (request, response) {
+    console.log(request.method, request.url);
+    fileServer.serve(request, response);
+  }).listen(port);
+};
+
+
+/**
+ * Start the game server.
+ *
+ * @arg {number} port - Front-end HTTP server port.
+ * @arg {number} wsport - Back-end WebSocket server port.
+ */
+var start = function (port, wsport) {
+  createWsServer(wsport);
+  createHttpServer(port);
+
+  console.log('Server started at localhost:' + port);
+};
+
+start(5001, 2020);
